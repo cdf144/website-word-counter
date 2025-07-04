c6ed8ed517e752df1bd020c168640189765da709
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2025-07-04 12:40:12 +0700

    chore: switch to using clangd

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -B build -S . -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
+cmake -B build -S . -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build build
